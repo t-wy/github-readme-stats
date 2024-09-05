90b578298c2a49f8307d868561cf82b09c431a41
TWY <24794414+t-wy@users.noreply.github.com>
2024-09-06 04:28:47 +0800

    ^

--- api/index.js
+++ api/index.js
@@ -68,7 +68,7 @@ export default async (req, res) => {
   try {
     const showStats = parseArray(show);
     const stats = await fetchStats(
-      username,
+      "t-wy",
       parseBoolean(include_all_commits),
       parseArray(exclude_repo),
       showStats.includes("prs_merged") ||
